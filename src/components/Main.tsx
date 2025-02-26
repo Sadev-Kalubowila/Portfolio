1f52c1a507194f5174b167967aac7115ff000653
SadevMK <111415549+SadevMK@users.noreply.github.com>
2025-02-26 09:39:03 +0530

    Update Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://raw.githubusercontent.com/Sadev-Kalubowila/Portfolio/refs/heads/main/src/assets/images/bg-light.png" alt="Avatar" />
+          <img src="https://www.linkedin.com/in/yujisato/" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">

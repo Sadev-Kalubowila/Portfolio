caeb1468c2f92b0216d0ca0909872b69dfbabff1
SadevMK <111415549+SadevMK@users.noreply.github.com>
2025-02-27 16:21:35 +0530

    Update Timeline Script

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,50 +16,50 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="MAY 2023 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Senior XR|VR Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Mediwave (Pvt) Ltd, Sri Lanka</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              Developed the world's first fully functional connected ambulance solution for the National Ambulance Service using Microsoft HoloLens 2, Vuzix Smart Glasses, and Azure services.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="AUG 2022 - MAY 2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Software Engineer- Unity3D</h3>
+            <h4 className="vertical-timeline-element-subtitle">LKSimulations (Pvt) Ltd, Sri Lanka</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              Developed various AR & VR applications from marker-based, markerless,web-bassed and social media AR,etc using Meta Quest VR headsets and Leap Motion controllers.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="APR 2021 - AUG 2022"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Solutions Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">X.O Concepts (Pvt) Ltd, Sri Lanka</h4>
             <p>
-              Full-stack Development, API Development, User Experience
+              Developed AR-IoT based industrial monitoring applications for leading textile factories.Enhased the 'XdotO App' company's official app adding some AR experiences.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
+            date="MAR 2019 - SEP 2019"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <h3 className="vertical-timeline-element-title">Training Electronics Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">A&T Labs (Pvt) Ltd, Sri Lanka</h4>
             <p>
-              Automation, Data Governance, Statistical Analysis
+              Worked on various electronics and research related projects.
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>

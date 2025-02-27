7f6ef4e33575551417c6745401a8a276e8d60206
SadevMK <111415549+SadevMK@users.noreply.github.com>
2025-02-27 15:08:35 +0530

    Update Expert scrtipt

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,44 +1,45 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faCubes, faVrCardboard, faCube } from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "React",
-    "TypeScript",
+    "Unity Engine",
+    "Unreal Engine",
+    "WebXR",
     "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
+    "C++/C#",
     "Python",
     "SQL",
-    "PostgreSQL",
+    "ARKit",
+    "ARCore",
+    "OpenXR",
+    "MRTK",
     "Postman"
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "HoloLens 2",
+    "Trimble",
+    "Meta Quest 3",
+    "Vuzix AR glass",
+    "Leap Motion Controller",
+    "Metterport Pro",
+    "LIDAR camera",
+    "Vuforia",
+    "Spark AR",
+    "Mind AR",
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "Blender",
+    "Solidworks",
+    "Mixamo",
+    "SketchUp",
+    "Photogrametry",
+    "Polycam",
 ];
 
 function Expertise() {
@@ -48,9 +49,9 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <FontAwesomeIcon icon={faCubes} size="3x"/>
+                    <h3>Full-Stack XR Development</h3>
+                    <p>I have built a diverse range of immersive applications across AR, VR, MR, and WebXR from the ground up. My expertise includes developing industrial-grade Mixed Reality applications integrated with IoT for industries such as emergency healthcare and apparel.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -60,9 +61,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faVrCardboard} size="3x"/>
+                    <h3>Hardware & Platforms</h3>
+                    <p>Skilled in creating and deploying cutting-edge XR applications across various devices and platforms. Experienced in integrating spatial computing and diverse interaction methods, leveraging AI and ML to enhance user experiences.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -72,9 +73,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <FontAwesomeIcon icon={faCube} size="3x"/>
+                    <h3>3D modeling & Animations</h3>
+                    <p>Expert in integrating optimized 3D models and animations into XR applications, ensuring high performance and realism. Proficient in 3D asset creation, rigging, and animation workflows for immersive experiences.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (

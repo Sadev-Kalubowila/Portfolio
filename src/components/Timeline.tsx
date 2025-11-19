ef95d34e96f4f5dd5567791879c1daa39758a676
SadevMK <111415549+SadevMK@users.noreply.github.com>
2025-11-19 07:19:55 +0530

    Timeline Update

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,7 +16,7 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="MAY 2023 - present"
+            date="MAY 2023 - APRIL 2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
